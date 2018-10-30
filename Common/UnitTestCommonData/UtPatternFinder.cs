cd55bfc692125f17a3e26c03460bcc6f9e44fe3b
Deno <denis.akopyan@seznam.cz>
2018-10-30 14:00:54 +0100

    Improve UT for PatternFinder

--- Common/UnitTestCommonData/UtPatternFinder.cs
+++ Common/UnitTestCommonData/UtPatternFinder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using UnitTestConstants;
 
@@ -11,11 +13,18 @@ namespace Common.Data.Tests
     [TestMethod, TestCategory(Constants.METHOD)]
     public void FindPattern()
     {
+      // Arrange
       var data = new List<string> { "234", "2301", "501", "01" };
       var finder = new PatternFinder(data);
+
+      // Act
       var result = finder.FindPattern();
 
-      Assert.AreEqual("234|(0|230|50)1", result);
+      // Assert
+      Assert.AreNotEqual(string.Empty, result);
+
+      var count = data.Count(x => Regex.IsMatch(x, result));
+      Assert.AreEqual(data.Count, count);
     }
   }
 }
