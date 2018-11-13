910cd7166c4acdede2e568d5ccbec28bbd480d6a
Deno <denis.akopyan@seznam.cz>
2018-11-13 15:30:47 +0100

    Add Common.Linq project

--- Common/CommonData/PatternFinder.cs
+++ Common/CommonData/PatternFinder.cs
@@ -1,24 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
+using Common.Linq;
 
 namespace Common.Data
 {
-  static class MyClass
-  {
-    public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
-    {
-      HashSet<TKey> seenKeys = new HashSet<TKey>();
-      foreach (TSource element in source)
-      {
-        if (seenKeys.Add(keySelector(element)))
-        {
-          yield return element;
-        }
-      }
-    }
-  }
-
   public class PatternFinder
   {
     #region Subclasses
