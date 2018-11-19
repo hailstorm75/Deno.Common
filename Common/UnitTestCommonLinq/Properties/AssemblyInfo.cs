1cbb4958b44b49dd721fac90f30783ef633a958e
Deno <denis.akopyan@seznam.cz>
2018-11-19 12:08:11 +0100

    Add UnitTestCommonLinq

---
+++ Common/UnitTestCommonLinq/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestCommonLinq")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestCommonLinq")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d43dcbe-d183-4335-9cee-1909cbcb9216")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Common/UnitTestCommonLinq/UtLinqExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestConstants;
+
+namespace Common.Linq.Tests
+{
+  [TestClass]
+  public class UtLinqExtensions
+  {
+    [TestMethod, TestCategory(Constants.METHOD)]
+    public void TestMethod1()
+    {
+    }
+  }
+}
