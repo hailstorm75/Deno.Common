eb4691ad80b148c1332347cb28defc2695827db8
Deno <denis.akopyan@seznam.cz>
2018-11-28 20:38:21 +0100

    Create CommonCore

---
+++ CommonCore/CommonMath/LongestLong.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Common.Math
+{
+  [Serializable]
+  public class LongestLong
+  {
+    #region Properties
+
+    /// <summary>
+    /// Number container
+    /// </summary>
+    public List<long> Values { get; }
+
+    public bool Positive { get; }
+
+    #endregion
+
+    #region Fields
+
+    public static readonly long MAX_VALUE = long.Parse("1" + new string('0', (long.MaxValue / 10).ToString().Length));
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public LongestLong() => Values = new List<long>();
+
+    /// <summary>
+    /// Variadic constructor
+    /// </summary>
+    /// <param name="list">Value to initialize with</param>
+    public LongestLong(params long[] list)
+    {
+      if (!ValidateNumbers(list)) throw new ArgumentException($"Only the first element from argument {nameof(list)} can have a negative value.");
+      Positive = list?.First() >= 0;
+      Values = AdjustList(list.Reverse().ToList());
+    }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="list">List of values to initialize from</param>
+    public LongestLong(List<long> list)
+    {
+      if (!ValidateNumbers(list)) throw new ArgumentException($"Only the first element from argument {nameof(list)} can have a negative value.");
+      Positive = list?.First() >= 0;
+      // TODO Optimize
+      Values = list != null ? AdjustList(list.AsEnumerable().Reverse().ToList()) : new List<long>{ 0 };
+    }
+
+    #endregion
+
+    #region Operators
+
+    #region Addition
+
+    public static LongestLong operator +(LongestLong a, long b) => a + new LongestLong(b);
+
+    public static LongestLong operator +(LongestLong a, LongestLong b)
+    {
+      var result = new List<long>();
+      Add(a, b);
+
+      return new LongestLong(result);
+    }
+
+    public static long operator +(long a, LongestLong b)
+    {
+      // TODO This can overflow
+      return a + b.Values.First();
+    }
+
+    #endregion
+
+    #region Subtraction
+
+    public static LongestLong operator -(LongestLong a, long b) => a - new LongestLong(b);
+
+    public static LongestLong operator -(LongestLong a, LongestLong b)
+    {
+      throw new NotImplementedException();
+    }
+
+    public static long operator -(long a, LongestLong b)
+    {
+      // TODO This can underflow
+      return a - b.Values.First();
+    }
+
+    #endregion
+
+    #region Multiplication
+
+    public static LongestLong operator *(LongestLong a, long b)
+    {
+      throw new NotImplementedException();
+    }
+
+    public static LongestLong operator *(LongestLong a, LongestLong b)
+    {
+      throw new NotImplementedException();
+    }
+
+    public static long operator *(long a, LongestLong b)
+    {
+      throw new NotImplementedException();
+    }
+
+    #endregion
+
+    #region Division
+
+    public static LongestLong operator /(LongestLong a, long b)
+    {
+      throw new NotImplementedException();
+    }
+
+    public static LongestLong operator /(LongestLong a, LongestLong b)
+    {
+      throw new NotImplementedException();
+    }
+
+    public static long operator /(long a, LongestLong b)
+    {
+      throw new NotImplementedException();
+    }
+
+    #endregion
+
+    #endregion
+
+    #region Methods
+
+    private static void LeftArgLargest(ref LongestLong x, ref LongestLong y)
+    {
+      if (x.Values.Count > y.Values.Count) return;
+
+      var tmp = x;
+      x = y;
+      y = tmp;
+    }
+
+    private static bool ValidateNumbers(IEnumerable<long> list) => list.Skip(1).All(num => num >= 0);
+
+    private static List<long> AdjustList(List<long> list)
+    {
+      long carry = 0;
+
+      list[list.Count - 1] = System.Math.Abs(list.Last());
+      for (var i = 0; i < list.Count; ++i)
+      {
+        if (list[i] < MAX_VALUE)
+        {
+          list[i] += carry;
+          carry = 0;
+          continue;
+        }
+
+        var tmp = list[i] / MAX_VALUE;
+        list[i] -= tmp * MAX_VALUE - carry;
+        carry = tmp;
+      }
+
+      if (carry != 0)
+        list.Add(carry);
+
+      return list;
+    }
+
+    /// <summary>
+    /// Recurvely adds list <paramref name="x"/> with list <paramref name="y"/>
+    /// </summary>
+    /// <param name="x">Left hand side argument</param>
+    /// <param name="y">Right hand side argument</param>
+    /// <param name="result">Operation result</param>
+    /// <param name="index">Recursion iteration index</param>
+    /// <param name="carryover">Calculation carryover</param>
+    private static void Add(LongestLong x, LongestLong y)
+    {
+      LeftArgLargest(ref x, ref y);
+    }
+
+    private List<long> Multiply(List<long> x, List<long> y)
+    {
+      if (x.Count > y.Count) throw new ArgumentException($"List {nameof(x)} must contain less elements than {nameof(y)}");
+
+      // Multiplying is just adding y times
+      // Under the assumption that the number of elements in y is less than the number of elements in x
+      // So now we actually need to access the number represented by list Y.
+      var result = new List<long>();
+
+      // TODO This solution is invalid
+      //for (int i = 0; i < y.Count; i++)
+      //  for (int j = 0; j < y[i] * (int)System.Math.Pow(long.MaxValue, (y.Count - i - 1)); j++)
+      //    product = Adding(product, Adding(x, x, x.Count, false), product.Count, false);
+
+      return result;
+    }
+
+    #endregion
+
+    public override string ToString()
+    {
+      var sb = new StringBuilder
+      {
+        Capacity = 1 + Values.Count
+      };
+
+      sb.Append(Positive ? string.Empty : "-").Append(string.Concat(Values.AsEnumerable().Reverse()));
+
+      return sb.ToString();
+    }
+  }
+}
---
+++ CommonCore/CommonMath/UniversalNumericOperation.cs
@@ -0,0 +1,182 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("UnitTestCommonMath")]
+namespace Common.Math
+{
+  internal static class UniversalNumericOperation
+  {
+    #region Extensions
+
+    #region Validators
+
+    public static bool IsNumber<T>(this T value)
+    {
+      var type = typeof(T);
+
+      return type == typeof(short)
+          || type == typeof(int)
+          || type == typeof(long)
+          || type == typeof(ushort)
+          || type == typeof(uint)
+          || type == typeof(ulong)
+          || type == typeof(float)
+          || type == typeof(double)
+          || type == typeof(decimal);
+    }
+
+    public static bool IsInteger<T>(this T value)
+    {
+      var type = typeof(T);
+      return type == typeof(short)
+          || type == typeof(int)
+          || type == typeof(long)
+          || type == typeof(ushort)
+          || type == typeof(uint)
+          || type == typeof(ulong);
+    }
+
+    public static bool IsSignedInteger<T>(this T value)
+    {
+      var type = typeof(T);
+      return type == typeof(short)
+          || type == typeof(int)
+          || type == typeof(long);
+    }
+
+    public static bool IsUnsignedInteger<T>(this T value)
+    {
+      var type = typeof(T);
+      return type == typeof(ushort)
+          || type == typeof(uint)
+          || type == typeof(ulong);
+    }
+
+    public static bool IsDecimal<T>(this T value)
+    {
+      var type = typeof(T);
+      return type == typeof(float)
+          || type == typeof(double)
+          || type == typeof(decimal);
+    }
+
+    #endregion
+
+    #region Operators
+
+    public static T Add<T>(this T x, T y) => Add<T, T>(x, y);
+    public static T Subtract<T>(this T x, T y) => Subtract<T, T>(x, y);
+    public static T Multiply<T>(this T x, T y) => Multiply<T, T>(x, y);
+    public static T Divide<T>(this T x, T y) => Divide<T, T>(x, y);
+    public static T Modulo<T>(this T x, T y) => Modulo<T, T, T>(x, y);
+
+    #endregion
+
+    #endregion
+
+    #region Operators
+
+    public static TResult Add<T1, T2, TResult>(T1 x, T2 y)
+    {
+      dynamic dx = x, dy = y;
+      return (TResult)(dx + dy);
+    }
+
+    public static TResult Add<T, TResult>(params T[] ts)
+    {
+      dynamic res = ts[0];
+      for (var i = 1; i < ts.Length; i++)
+        res += (dynamic)ts[i];
+
+      return (TResult)res;
+    }
+
+    public static TResult Subtract<T1, T2, TResult>(T1 x, T2 y)
+    {
+      dynamic dx = x, dy = y;
+      return (TResult)(dx - dy);
+    }
+
+    public static TResult Subtract<T, TResult>(params T[] ts)
+    {
+      dynamic res = ts[0];
+      for (var i = 1; i < ts.Length; i++)
+        res -= (dynamic)ts[i];
+
+      return (TResult)res;
+    }
+
+    public static TResult Multiply<T1, T2, TResult>(T1 x, T2 y)
+    {
+      dynamic dx = x, dy = y;
+      return (TResult)(dx * dy);
+    }
+
+    public static TResult Multiply<T, TResult>(params T[] ts)
+    {
+      dynamic res = ts[0];
+      for (var i = 1; i < ts.Length; i++)
+        res *= (dynamic)ts[i];
+
+      return (TResult)res;
+    }
+
+    public static TResult Divide<T1, T2, TResult>(T1 x, T2 y)
+    {
+      dynamic dx = x, dy = y;
+      return (TResult)(dx / dy);
+    }
+
+    public static TResult Divide<T, TResult>(params T[] ts)
+    {
+      dynamic res = ts[0];
+      for (var i = 1; i < ts.Length; i++)
+        res /= (dynamic)ts[i];
+
+      return (TResult)res;
+    }
+
+    public static TResult Modulo<T1, T2, TResult>(T1 x, T2 y) => (TResult)((dynamic)x % (dynamic)y);
+
+    public static bool IsEqual<T1, T2>(T1 x, T2 y) => (dynamic)x == (dynamic)y;
+    public static bool IsLess<T1, T2>(T1 x, T2 y) => (dynamic)x < (dynamic)y;
+    public static bool IsLessEqual<T1, T2>(T1 x, T2 y) => (dynamic)x <= (dynamic)y;
+    public static bool IsGreater<T1, T2>(T1 x, T2 y) => (dynamic)x > (dynamic)y;
+    public static bool IsGreaterEqual<T1, T2>(T1 x, T2 y) => (dynamic)x >= (dynamic)y;
+
+    #endregion
+
+    #region Methods
+
+    public static T Abs<T>(T value) => System.Math.Abs((dynamic)value);
+
+    public static T GetMaxValue<T>(T value)
+    {
+      var type = typeof(T).Name;
+      switch (type)
+      {
+        case "Int16": return (T)(dynamic) short.MaxValue;
+        case "Int32": return (T)(dynamic) int.MaxValue;
+        case "Int64": return (T)(dynamic) long.MaxValue;
+        case "Single": return (T)(dynamic) float.MaxValue;
+        case "Double": return (T)(dynamic) double.MaxValue;
+        default: return default(T);
+      }
+    }
+
+    public static T GetMinValue<T>(T value)
+    {
+      var type = typeof(T).Name;
+      switch (type)
+      {
+        case "Int16": return (T)(dynamic) short.MinValue;
+        case "Int32": return (T)(dynamic) int.MinValue;
+        case "Int64": return (T)(dynamic) long.MinValue;
+        case "Single": return (T)(dynamic) float.MinValue;
+        case "Double": return (T)(dynamic) double.MinValue;
+        default: return default(T);
+      }
+    }
+
+    #endregion
+  }
+}
