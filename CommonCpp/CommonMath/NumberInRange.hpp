96b1075fc3a2d0adbd279f21c3d30687c62767bf
Deno <denis.akopyan@seznam.cz>
2018-12-23 18:58:16 +0100

    [skip ci] Create solution, project. Finish NumberInRange template

--- CommonCpp/CommonMath/NumberInRange.hpp
+++ CommonCpp/CommonMath/NumberInRange.hpp
@@ -1,61 +1,151 @@
+#pragma once
 #include <type_traits>
+#include <stdexcept>
+#include <sstream>
 
-namespace Common::Math
+#define NAMEOF(x) std::string(#x)
+
+namespace Common
 {
-  template <class T,
-  class = class std::enable_if<std::is_arithmetic<T>::value && std::is_integral<T>::value, T>::type>
-  class NumberInRange
+  namespace Math
   {
-    /*
-     * \brief Value in range
-    */
-    T m_value;
-    /*
-     * \brief Range minimum
-    */
-    T m_min;
-    /*
-     * \brief Range maximum
-    */
-    T m_max;
-
-    const T m_rangeLen;
-
-  public:
-    NumberInRange<T>(const T &value, const T &min, const T &max)
-      : m_value(value), m_min(min), m_max(max), m_rangeLen(max /*TODO*/)
+    /**
+     * \brief Keeps an integer value in a given range
+     * \tparam T Type of value. Anything but integer types are prohibited.
+     */
+    template <typename T, std::enable_if_t<std::is_arithmetic<T>::value && std::is_integral<T>::value>* = nullptr>
+    class NumberInRange
     {
-      if (min > max) throw std::invalid_argument("Argument min cannot be greater than argument max.");
-    }
+      /**
+       * \brief Range minimum
+      */
+      const T m_min;
+      /**
+       * \brief Range maximum
+      */
+      const  T m_max;
+      /**
+      * \brief Distance between the Minimum and Maximum
+      */
+      const T m_rangeLen;
 
-    ~NumberInRange<T>() {}
+      /**
+       * \brief Value in range
+      */
+      T m_value;
 
-    const T & GetMin() const { return m_min; }
-
-    const T & GetMax() const { return m_max; }
-
-    const T & GetValue() const { return m_value; }
-    const T & SetValue(const T & value) { m_value = AdjustValue(value); }
-
-  private:
-    const T & AdjustValue(const T & value)
-    {
-      if (value >= m_min && value <= m_max)
-        return value;
-
-      if (value > m_min)
+    public:
+      /**
+       * \brief Default constructor
+       * \param value Value to hold
+       * \param min Range minimum
+       * \param max Range maximum
+       */
+      NumberInRange(const T & value, const T & min, const T & max)
+        : m_min(min), m_max(max), m_rangeLen(abs(abs(m_min) - abs(m_max)) + 1), m_value(AdjustValue(value))
       {
-        if (m_min < 0)
-          return ((value - m_min) % m_rangeLen) + m_min; // TODO Abs
-        auto remainder = value & m_rangeLen;
-        return remainder + m_min;
+        static_assert(!std::is_same<T, double>::value && !std::is_same<T, float>::value, "NumberInRange: T cannot be of a floating point type.");
+        if (min == max) throw std::invalid_argument("Argument " + NAMEOF(min) + " cannot be equal to argument " + NAMEOF(max) + ".");
+        if (min > max) throw std::invalid_argument("Argument " + NAMEOF(min) + " cannot be greater than argument " + NAMEOF(max) + ".");
       }
-      else
+
+      /**
+       * \brief Getter for the Min property
+       * \return Range Minimum
+       */
+      const T & GetMin() const { return m_min; }
+
+      /**
+       * \brief Getter for the Max property
+       * \return Range Maximum
+       */
+      const T & GetMax() const { return m_max; }
+
+      /**
+       * \brief Getter for the Value property
+       * \return Value
+       */
+      const T & GetValue() const { return m_value; }
+      /**
+       * \brief Setter for the Value property
+       * \param value New value to set
+       */
+      void SetValue(const T & value) { m_value = AdjustValue(value); }
+
+      static T AdjustValue(const T & value, const T & min, const T & max)
       {
-        auto remainder = (value - m_min) % m_rangeLen;
+        return NumberInRange<T>(value, min, max).GetValue();
+      }
+
+      T operator +(const T & other) noexcept
+      {
+        return AdjustValue(m_value + AdjustValue(other, m_min, m_max));
+      }
+      T operator +(const NumberInRange<T> & other) noexcept
+      {
+        return AdjustValue(m_value + AdjustValue(other.m_value, m_min, m_max));
+      }
+
+      T operator -(const T & other) noexcept
+      {
+        return AdjustValue(m_value - AdjustValue(other, m_min, m_max));
+      }
+      T operator -(const NumberInRange<T> & other) noexcept
+      {
+        return AdjustValue(m_value - AdjustValue(other.m_value, m_min, m_max));
+      }
+
+      T operator *(const T & other) noexcept
+      {
+        return AdjustValue(m_value * AdjustValue(other, m_min, m_max));
+      }
+      T operator *(const NumberInRange<T> & other) noexcept
+      {
+        return AdjustValue(m_value * AdjustValue(other.m_value, m_min, m_max));
+      }
+
+      T operator /(const T & other)
+      {
+        return AdjustValue(m_value / AdjustValue(other, m_min, m_max));
+      }
+      T operator /(const NumberInRange<T> & other)
+      {
+        return AdjustValue(m_value / AdjustValue(other.m_value, m_min, m_max));
+      }
+
+      T operator %(const T & other)
+      {
+        return AdjustValue(m_value % AdjustValue(other, m_min, m_max));
+      }
+      T operator %(const NumberInRange<T> & other)
+      {
+        return AdjustValue(m_value % AdjustValue(other.m_value, m_min, m_max));
+      }
+
+      std::string ToString()
+      {
+        std::ostringstream oss;
+        oss << m_value;
+        return oss.str();
+      }
+
+    private:
+      T AdjustValue(const T & value)
+      {
+        if (value >= m_min && value <= m_max)
+          return value;
+
+        if (value > m_min)
+        {
+          if (m_min < 0)
+            return (abs(value - m_min) % m_rangeLen) + m_min; // TODO Abs
+          auto remainder = value % m_rangeLen;
+          return remainder + m_min;
+        }
+
+        auto remainder = abs(value - m_min) % m_rangeLen;
         return remainder == 0 ? m_min : m_rangeLen - remainder + m_min;
       }
-    }
-  };
+    };
+  }
 }
-// namespace Common::Math
