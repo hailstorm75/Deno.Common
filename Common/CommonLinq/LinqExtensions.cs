85e48fbd87f520985a83fcf97c17eeb32e2b99a6
Deno <denis.akopyan@seznam.cz>
2019-02-16 22:40:44 +0100

    Add ChunkBy extension

--- Common/CommonLinq/LinqExtensions.cs
+++ Common/CommonLinq/LinqExtensions.cs
@@ -15,7 +15,8 @@ namespace Common.Linq
           yield return element;
     }
 
-    public static IEnumerable<TOut> ForEachDo<TSource, TOut>(this IEnumerable<TSource> source, Func<TSource, TOut> func) => source.Select(func);
+    public static IEnumerable<TOut> ForEachDo<TSource, TOut>(this IEnumerable<TSource> source, Func<TSource, TOut> func)
+      => source.Select(func);
 
     public static void Sort<T>(this ObservableCollection<T> observable) where T : IComparable<T>, IEquatable<T>
     {
@@ -32,6 +33,14 @@ namespace Common.Linq
         }
         else ptr++;
       }
+    }
+
+    public static IEnumerable<IEnumerable<T>> ChunkBy<T>(this IEnumerable<T> source, int chunkSize)
+    {
+      return source
+        .Select((x, i) => new { Index = i, Value = x })
+        .GroupBy(x => x.Index / chunkSize)
+        .Select(x => x.Select(v => v.Value));
     }
   }
 }
