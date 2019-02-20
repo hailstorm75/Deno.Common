9ca0cae1273c2a5271fe86aff2fbfcba8f295c16
Deno <denis.akopyan@seznam.cz>
2019-02-20 21:48:59 +0100

    Update dependencies, add nuget support

--- Common/CommonMath/Properties/AssemblyInfo.cs
+++ Common/CommonMath/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CommonMath")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".NET Framework library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Denis Akopyan")]
 [assembly: AssemblyProduct("CommonMath")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
